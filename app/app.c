df624251a84f57e2bf7e6897e340c462c4f652d2
Paul Jasek <papj2000@gmail.com>
2018-11-29 14:01:01 -0500

    added option to kill processes and option to sort by memory

--- app/app.c
+++ app/app.c
@@ -11,6 +11,9 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <libgen.h>
+#include <signal.h>
+#include <math.h>
+
 
 #define HIJACKED_SYSCALL __NR_tuxcall
 
@@ -21,7 +24,9 @@ struct process {
   long long previous_time;
   double cpu_usage;
   double cum_cpu_usage;
-  char memory_usage[1000];
+  long long cum_memory_usage;
+  long long memory_usage;
+  char memory_usage_string[100];
   char name[1000];
   GtkTreeIter iter;
 };
@@ -80,15 +85,16 @@ long long get_process_cpu_jiffies(int pid) {
   return utime + cutime + stime + cstime;
 }
 
-int get_process_memory_usage(int pid, char * memory_usage) {
+long long get_process_memory_usage(int pid) {
   char filename[200];
   sprintf(filename, "/proc/%d/status", pid);
   FILE *fp = fopen(filename, "r");
   if (!fp) return 1;
-  char *line;
+  char *line, *memory_usage_str;
   size_t len = 1024;
 
   line = malloc(len);
+  memory_usage_str = malloc(len);
 
 	
   while (1)
@@ -101,14 +107,26 @@ int get_process_memory_usage(int pid, char * memory_usage) {
 	}
 	if (!strncmp(line, "VmSize:", 7))
 	{
-		strcpy(memory_usage, strdup(&line[8]));
-		strtok(memory_usage, "\n");
+		strcpy(memory_usage_str, strdup(&line[8]));
+		strtok(memory_usage_str, " kB");
 		break;
 	}
   }
 
+  long long memory_usage = atoll(memory_usage_str) * 1024;
+
   free(line);
+  free(memory_usage_str);
   fclose(fp);
+
+  return memory_usage;
+}
+
+char* MEM_SIZES[] = {"Bytes", "kB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"};
+void format_memory(long long bytes, char *string) {
+  int i = log2(bytes)/log2(1024);
+  float decimal = 1.0 * bytes / pow(1024, i);
+  sprintf(string, "%.1f %s", decimal, MEM_SIZES[i]);
 }
 
 int get_process_name(int pid, char *name) {
@@ -137,6 +155,7 @@ enum
    PROCESS_NAME_COLUMN,
    CPU_USAGE_COLUMN,
    MEMORY_USAGE_COLUMN,
+   MEMORY_USAGE_NUMBER_COLUMN,
    N_COLUMNS
 };
 
@@ -150,7 +169,8 @@ GNode * build_process_tree(pid_t parent_pid, GtkTreeIter *parent_iter) {
 		      PID_COLUMN, process_data->pid,
                       PROCESS_NAME_COLUMN, process_data->name,
                       CPU_USAGE_COLUMN, process_data->cum_cpu_usage * 100,
-                      MEMORY_USAGE_COLUMN, process_data->memory_usage,
+                      MEMORY_USAGE_COLUMN, process_data->memory_usage_string,
+		      MEMORY_USAGE_NUMBER_COLUMN, process_data->cum_memory_usage,
                       -1);
 
   GNode *parent = g_node_new (process_data);
@@ -253,9 +273,37 @@ gboolean update_cum_cpu_utilization(GNode *node, gpointer info) {
 gboolean update_memory_utilization(GNode *node, gpointer info) {
   struct process *data;
   data = (struct process *) node->data;
-  get_process_memory_usage(data->pid, data->memory_usage);
+  data->memory_usage = get_process_memory_usage(data->pid);
 
   return 0;
+}
+
+gboolean calculate_cum_memory_utilization(GNode *node, gpointer sum) {
+    struct process *data;
+    data = (struct process *) node->data;
+
+    long long *val = (long long *) sum;
+    *val += data->memory_usage;
+
+    return 0;
+}
+
+gboolean update_cum_memory_utilization(GNode *node, gpointer info) {
+    struct process *data;
+    data = (struct process *) node->data;
+
+    data->cum_memory_usage = 0;
+
+    g_node_traverse (node,
+                 G_IN_ORDER,
+                 G_TRAVERSE_ALL,
+                 -1,
+                 calculate_cum_memory_utilization,
+                 &data->cum_memory_usage);
+
+    format_memory(data->cum_memory_usage, data->memory_usage_string);
+    
+    return 0;
 }
 
 gboolean update_process_name(GNode *node, gpointer info) {
@@ -275,7 +323,8 @@ gboolean update_gtk_tree(GNode *node, gpointer info) {
 		      PID_COLUMN, data->pid,
                       PROCESS_NAME_COLUMN, data->name,
                       CPU_USAGE_COLUMN, data->cum_cpu_usage * 100,
-                      MEMORY_USAGE_COLUMN, data->memory_usage,
+                      MEMORY_USAGE_COLUMN, data->memory_usage_string,
+		      MEMORY_USAGE_NUMBER_COLUMN, data->cum_memory_usage,
                       -1);
 
   return 0;
@@ -312,6 +361,12 @@ gboolean update_utilization(GtkWidget * data)
                  update_memory_utilization,
                  NULL);
 
+  g_node_traverse (process_tree,
+                 G_IN_ORDER,
+                 G_TRAVERSE_ALL,
+                 -1,
+                 update_cum_memory_utilization,
+                 NULL);
 
   g_node_traverse (process_tree,
                  G_IN_ORDER,
@@ -347,6 +402,22 @@ tree_selection_changed_cb (GtkTreeSelection *selection, gpointer data)
         }
 }
 
+/* Our usual callback function */
+void kill_button_callback( GtkWidget *widget, gpointer   data )
+{
+  kill(selected_pid, SIGTERM);
+}
+
+void toggle_memory_sort(GtkWidget *widget, gpointer data)
+{
+  gtk_tree_sortable_set_sort_column_id (GTK_TREE_SORTABLE(store), MEMORY_USAGE_NUMBER_COLUMN, GTK_SORT_DESCENDING);
+}
+
+void toggle_cpu_sort(GtkWidget *widget, gpointer data)
+{
+  gtk_tree_sortable_set_sort_column_id (GTK_TREE_SORTABLE(store), CPU_USAGE_COLUMN, GTK_SORT_DESCENDING);
+}
+
 static void
 activate (GtkApplication *app,
           gpointer        user_data)
@@ -358,12 +429,12 @@ activate (GtkApplication *app,
 					    G_TYPE_INT,
                                             G_TYPE_STRING,
                                             G_TYPE_DOUBLE,
-                                            G_TYPE_STRING);
+                                            G_TYPE_STRING,
+					    G_TYPE_ULONG
+  );
 
   GtkTreeSortable *sortable = GTK_TREE_SORTABLE(store);
-  //gtk_tree_sortable_set_sort_column_id (sortable, PROCESS_NAME_COLUMN, GTK_SORT_ASCENDING);
   gtk_tree_sortable_set_sort_column_id (sortable, CPU_USAGE_COLUMN, GTK_SORT_DESCENDING);
-  //gtk_tree_sortable_set_sort_column_id (sortable, MEMORY_USAGE_COLUMN, GTK_SORT_ASCENDING);
 
   window = gtk_application_window_new (app);
   gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW(window), GTK_POLICY_AUTOMATIC,
@@ -377,48 +448,54 @@ activate (GtkApplication *app,
   tree = gtk_tree_view_new_with_model (GTK_TREE_MODEL (store));
 
   GtkCellRenderer *renderer, *progress_renderer;
-  GtkTreeViewColumn *column;
+  GtkTreeViewColumn *name_column, *cpu_usage_column, *memory_usage_column;
 
   renderer = gtk_cell_renderer_text_new ();
   progress_renderer = gtk_cell_renderer_progress_new();
 
-  column = gtk_tree_view_column_new_with_attributes ("Process Name",
+  name_column = gtk_tree_view_column_new_with_attributes ("Process Name",
                                                      renderer,
                                                      "text", PROCESS_NAME_COLUMN,
                                                      NULL);
-  gtk_tree_view_append_column (GTK_TREE_VIEW (tree), column);
+  gtk_tree_view_append_column (GTK_TREE_VIEW (tree), name_column);
 
-  column = gtk_tree_view_column_new_with_attributes ("CPU Usage",
+  cpu_usage_column = gtk_tree_view_column_new_with_attributes ("CPU Usage",
                                                      progress_renderer,
                                                      "value", CPU_USAGE_COLUMN,
                                                      NULL);
-  gtk_tree_view_append_column (GTK_TREE_VIEW (tree), column);
+  gtk_tree_view_append_column (GTK_TREE_VIEW (tree), cpu_usage_column);
 
-  column = gtk_tree_view_column_new_with_attributes ("Memory Usage",
+  memory_usage_column = gtk_tree_view_column_new_with_attributes ("Memory Usage",
                                                      renderer,
                                                      "text", MEMORY_USAGE_COLUMN,
                                                      NULL);
-  gtk_tree_view_append_column (GTK_TREE_VIEW (tree), column);
+  gtk_tree_view_append_column (GTK_TREE_VIEW (tree), memory_usage_column);
+
+  gtk_tree_view_set_headers_clickable(tree, TRUE);
+  g_signal_connect (G_OBJECT (cpu_usage_column), "clicked", G_CALLBACK (toggle_cpu_sort), NULL);
+  g_signal_connect (G_OBJECT (memory_usage_column), "clicked", G_CALLBACK (toggle_memory_sort), NULL);
 
   gtk_container_add (GTK_CONTAINER (scwin), tree);
 
   GtkWidget *action_bar = gtk_action_bar_new();
   GtkWidget *button_box = gtk_vbox_new(TRUE, 100);
 
-  kill_button = gtk_button_new_with_label("Kill");
-  gtk_widget_set_size_request (kill_button,200,40);
+  kill_button = gtk_button_new_with_label("End Task");
 
-  gtk_box_pack_start(button_box, kill_button, FALSE, FALSE, 0);
-  gtk_action_bar_pack_start (action_bar, button_box);
+  /* Connect the "clicked" signal of the button to our callback */
+  g_signal_connect (G_OBJECT (kill_button), "clicked",
+                        G_CALLBACK (kill_button_callback), NULL);
+
+  gtk_widget_set_size_request (kill_button,100,25);
+
+  gtk_box_pack_start((GtkBox *) button_box, kill_button, FALSE, FALSE, 0);
+  gtk_action_bar_pack_start ((GtkActionBar *) action_bar, button_box);
 
   GtkWidget *box = gtk_vbox_new(FALSE, 0);
   gtk_container_add (GTK_CONTAINER (window), box);
 
-  gtk_box_pack_start (box,
-                 action_bar, FALSE, FALSE, 0);
-
-  gtk_box_pack_start (box,
-                 scwin, TRUE, TRUE, 0);
+  gtk_box_pack_start ((GtkBox *) box, scwin, TRUE, TRUE, 0);
+  gtk_box_pack_start ((GtkBox *) box, action_bar, FALSE, FALSE, 0);
 
   /* Setup the selection handler */
   GtkTreeSelection *select;
@@ -432,6 +509,10 @@ activate (GtkApplication *app,
   gtk_widget_show_all (window);
 
   process_tree = build_process_tree(1, NULL);
+  gtk_tree_view_expand_row (GTK_TREE_VIEW(tree),
+			    gtk_tree_model_get_path (store,
+                            			     &(((struct process *) process_tree->data)->iter)),
+                            FALSE);
   update_utilization(NULL);
   g_timeout_add(2000, (GSourceFunc) update_utilization, NULL);
 }
